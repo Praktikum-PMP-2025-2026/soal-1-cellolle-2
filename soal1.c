a599410703efe30429bd8892c129c2685a967ec6
cellolle <marcellopandiangan@gmail.com>
2026-04-20 10:40:03 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -2,18 +2,18 @@
 
 int main() {
     int N;
-    if (scanf("%d", &N) != 1) return 0;
+    if (scanf("%d", &N));
 
     int array[N];
     for (int i = 0; i < N; i++) {
-        if (scanf("%d", &array[i]) != 1) break;
+        if (scanf("%d", &array[i]));
     }
     for (int i = 0; i < N; i++) {
         if (array[i] == -1) {
             int kiri = -1;
             int kanan = -1;
             if (i > 0) {
-                kiri    = array[i - 1];
+                kiri = array[i - 1];
             }
             for (int j = i + 1; j < N; j++) {
                 if (array[j] != -1) {
@@ -39,8 +39,8 @@ int main() {
         if (array[i] > 0) {
             MAX_SUM += array[i];
         }
-        printf("%d%s", array[i], (i == N - 1) ? "" : " ");
+        printf ("%d ",array[i]);
     }
-    printf("\nMAX_SUM %d\n", MAX_SUM);
+    printf("\nMAX_SUM %d", MAX_SUM);
     return 0;
 }
