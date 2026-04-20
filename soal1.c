18bff45f2a7899823b18b838716b14c6660593ab
cellolle <marcellopandiangan@gmail.com>
2026-04-20 10:43:11 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,51 +1,52 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 1 - Overview C Language
+ *   Hari dan Tanggal    : Senin, 20 April 2026
+ *   Nama (NIM)          : Marcello Menata Pandiangan (13224069)
+ *   Nama File           : tp2_2.c
+ *   Deskripsi           : Pemulihan Segmen Fragmen
+ * 
+ */
 
-int main() {
+ #include <stdio.h>
+ #include <string.h>
+   
+ int main(){
+    int MAX_SUM = 0;
     int N;
-    if (scanf("%d", &N));
-
+    scanf("%d", &N);
     int array[N];
     for (int i = 0; i < N; i++) {
-        if (scanf("%d", &array[i]));
+        scanf("%d", &array[i]);
     }
-    for (int i = 0; i < N; i++) {
+    if (array[N-1] == -1 && array[N-2] == -1) {
+        array[N-1] = 0;
+    }
+    for (int i = N-1; i > 0; i--) {
         if (array[i] == -1) {
-            int kiri = -1;
-            int kanan = -1;
-            if (i > 0) {
-                kiri = array[i - 1];
-            }
-            for (int j = i + 1; j < N; j++) {
-                if (array[j] != -1) {
-                    kanan = array[j];
-                    break;
-                }
-            }
-            if (kiri != -1 && kanan != -1) {
-                array[i] = (kiri + kanan) / 2;
-            } else if (kiri != -1) {
-                array[i] = kiri;
-            } else if (kanan != -1) {
-                array[i] = kanan;
-            } else {
+            if (array[i-1] != -1 && array[i+1] != -1) {
+                array[i] = (array[i+1] + array [i-1])/2;
+            }  else if (array[i-1] != -1 && array[i+1] != -1) {
+                array[i] = (array[i+1] + array [i-1])/2;
+            } else if (array[i+1] != -1 && array[i-1] == -1) {
+                array[i] = array[i+1] ;
+            } else if (array[i+1] == -1 && array[i-1] != -1) {
+                array[i] = array[i-1] ;
+            } else if (array[i+1] == -1 && (array[i-1] == -1)) {
                 array[i] = 0;
             }
         }
     }
-
-    int MAX_SUM = 0;
-    printf("\nRECOVERED ");
-    for (int i = 0; i < N-1; i++) {
-        if (array[i] > 0) {
-            MAX_SUM += array[i];
-        }
+    if (array[0] == -1) {
+        array[0] = array[1];
+    }
+    printf ("\nRECOVERED ") ;
+    int i = 0;
+    for (i = 0; i < N-1; i++) {
         printf ("%d ",array[i]);
+        MAX_SUM = MAX_SUM + array[i];
     }
     printf ("%d\n",array[N-1]);
-    if (array[N-1] > 0) {
-        MAX_SUM += array[N-1];
-    
+    MAX_SUM = MAX_SUM + array[N-1];
     printf("MAX_SUM %d",MAX_SUM);
-    printf("\nMAX_SUM %d", MAX_SUM);
     return 0;
 }
